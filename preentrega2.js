2b7af05bf20b1adad8c3404d801c710a3c90c0c5
kevinlorea <kevinlorea2018@gmail.com>
2023-10-26 08:52:14 -0300

    modifico codigo, uso unicamente dom eventos y ls

--- preentrega2.js
+++ preentrega2.js
@@ -1,61 +1,35 @@
-// asistente financiero mensual //
+
+function calcularBalance() {
+  const ingresos = parseFloat(localStorage.getItem('ingreso')) || 0;
+  const gastosFijos = parseFloat(localStorage.getItem('gastoFijo')) || 0;
+  const gastosVariables = parseFloat(localStorage.getItem('gastoVariable')) || 0;
+  
+  const balance = ingresos - (gastosFijos + gastosVariables);
+  
+  return balance;
+}
 
 
-// funcion para obtener la cantidad total de ingresos //
-function obtenerIngresos() {
-    return parseFloat(prompt("Ingrese sus ingresos mensuales:"));
-  }
-  
-  // funcion para obtener los gastos fijos //
-  function obtenerGastosFijos() {
-    const gastosFijos = [];
-    while (true) {
-      const gastoFijo = prompt("Ingrese a que corresponde el gasto fijo (o escriba 'fin' para terminar):");
-      if (gastoFijo.toLowerCase() === "fin") {
-        break;
-      }
-      const cantidad = parseFloat(prompt(`Ingrese la cantidad de ${gastoFijo}:`));
-      gastosFijos.push({ nombre: gastoFijo, cantidad });
-    }
-    return gastosFijos;
-  }
-  
-  // funcion para obtener los gastos variables //
-  function obtenerGastosVariables() {
-    const gastosVariables = [];
-    while (true) {
-      const gastoVariable = prompt("Ingrese a que corresponde el gasto variable (o escriba 'fin' para terminar):");
-      if (gastoVariable.toLowerCase() === "fin") {
-        break;
-      }
-      const cantidad = parseFloat(prompt(`Ingrese la cantidad de ${gastoVariable}:`));
-      gastosVariables.push({ nombre: gastoVariable, cantidad });
-    }
-    return gastosVariables;
-  }
-  
-  // funcion para calcular el saldo después de los gastos //
-  function calcularSaldoTotal(ingresos, gastosFijos, gastosVariables) {
-    const totalGastosFijos = gastosFijos.reduce((total, gasto) => total + gasto.cantidad, 0);
-    const totalGastosVariables = gastosVariables.reduce((total, gasto) => total + gasto.cantidad, 0);
-    return ingresos - totalGastosFijos - totalGastosVariables;
-  }
-  
-  // obtener los datos del usuario //
-  const ingresosMensuales = obtenerIngresos();
-  const gastosFijosMensuales = obtenerGastosFijos();
-  const gastosVariablesMensuales = obtenerGastosVariables();
-  
-  // calcular el saldo después de los gastos //
-  const saldoTotal = calcularSaldoTotal(ingresosMensuales, gastosFijosMensuales, gastosVariablesMensuales);
-  
-  // mostrar el resultado //
-  console.log("Resumen financiero mensual:");
-  console.log(`Ingresos: $${ingresosMensuales.toFixed(2)}`);
-  console.log("Gastos Fijos:");
-  gastosFijosMensuales.forEach((gasto) => console.log(`${gasto.nombre}: $${gasto.cantidad.toFixed(2)}`));
-  console.log("Gastos Variables:");
-  gastosVariablesMensuales.forEach((gasto) => console.log(`${gasto.nombre}: $${gasto.cantidad.toFixed(2)}`));
-  console.log(`Saldo Total: $${saldoTotal.toFixed(2)}`); 
+document.getElementById('registrar').addEventListener('click', function () {
+  const tipo = document.getElementById('tipo').value;
+  const monto = parseFloat(document.getElementById('monto').value);
+  const descripcion = document.getElementById('descripcion').value;
 
-  //
+  if (isNaN(monto)) {
+      alert('Por favor, ingrese un monto válido.');
+      return;
+  }
+ 
+ 
+  if (tipo === 'ingreso') {
+      localStorage.setItem('ingreso', monto);
+  } else if (tipo === 'gastoFijo') {
+      localStorage.setItem('gastoFijo', monto);
+  } else if (tipo === 'gastoVariable') {
+      localStorage.setItem('gastoVariable', monto);
+  }
+
+  
+  const balance = calcularBalance();
+  document.getElementById('balance').textContent = balance;
+});
